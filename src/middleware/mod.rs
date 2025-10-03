0388d673d4456529e1618505da8a3c1f7296037d
chandan0000 <kumarchandan41u@gmail.com>
2025-10-03 21:42:03 +0530

    fix some issue
    some issue fix

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,1 +1,2 @@
 pub mod auth_middleware;
+pub mod auth_handler;
--- src/service/socket_service.rs
+++ src/service/socket_service.rs
@@ -2,7 +2,6 @@ use dashmap::DashMap;
 use once_cell::sync::Lazy;
 use serde::Deserialize;
 use socketioxide::extract::{Data, SocketRef};
-use std::sync::Mutex;
 use tracing::info;
 
 #[derive(Debug, Deserialize, Clone)]
@@ -11,17 +10,15 @@ pub struct ConnectAuth {
     user_type: String,
 }
 
-static USER_INFO: Lazy<Mutex<DashMap<String, ConnectAuth>>> =
-    Lazy::new(|| Mutex::new(DashMap::new()));
+static USER_INFO: Lazy<DashMap<String, ConnectAuth>> = Lazy::new(|| DashMap::new());
+static USER_SOCKET: Lazy<DashMap<String, String>> = Lazy::new(|| DashMap::new());
 
 pub async fn on_connect(socket: SocketRef, Data(auth): Data<ConnectAuth>) {
     let socket_id = socket.id.to_string();
     let user_id = auth.user_id.clone();
 
-    {
-        let user_info = USER_INFO.lock().unwrap();
-        user_info.insert(socket_id.clone(), auth.clone());
-    }
+    USER_SOCKET.insert(user_id.clone(), socket_id.clone());
+    USER_INFO.insert(user_id.clone(), auth.clone());
 
     println!("🟢 [PRINT] Socket connected: {}", socket_id);
     println!(
@@ -34,8 +31,14 @@ pub async fn on_connect(socket: SocketRef, Data(auth): Data<ConnectAuth>) {
 
     socket.on_disconnect(move |socket: SocketRef| {
         println!("🔴 [PRINT] Socket disconnected: {}", socket.id);
+
         let socket_id = socket.id.to_string();
-        let user_info = USER_INFO.lock().unwrap();
-        user_info.remove(&socket_id);
+
+        if let Some(entry) = USER_SOCKET.iter().find(|kv| kv.value() == &socket_id) {
+            let user_id = entry.key().clone();
+
+            USER_INFO.remove(&user_id);
+            USER_SOCKET.remove(&user_id);
+        }
     });
 }
